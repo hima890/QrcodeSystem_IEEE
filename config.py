cc338332d7d19c8e039c6fd3c1f4b29005011aab
Hima890 <hfibrahim90@gmail.com>
2021-09-05 07:55:23 +0200

    efee

--- config.py
+++ config.py
@@ -1,15 +1,12 @@
 # the configration for databas path and the screet kay
-import os
-
-
 class Config:
     SECRET_KEY = "INDF87fdff"
-    SQLALCHEMY_DATABASE_URI =  os.environ['DATABASE_URL']
+    SQLALCHEMY_DATABASE_URI = "postgres://xyxigswgkwnskx:7f53937ae8c08540aae156a50fefec439ed64804c751d13a6c01b936650b271a@ec2-18-215-44-132.compute-1.amazonaws.com:5432/d2bk49n0u1olqc"
     MYSQL_DATABASE_CHARSET = 'utf8mb4'
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_DEFAULT_SENDER = "IEEENB@gmail.com"
-    MAIL_USERNAME = os.environ['MAIL_USERNAME']
-    MAIL_PASSWORD = os.environ['MAIL_PASSWORD']
+    MAIL_USERNAME = 'hfibrahim90@gmail.com'
+    MAIL_PASSWORD = 'majoja12345@@'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
   
