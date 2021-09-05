6855ce4ae8d8fbfb41a46f8999ef24c52699f9b7
Hima890 <hfibrahim90@gmail.com>
2021-09-05 08:43:17 +0200

    make it better 23.z0xxssx

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 # the configration for databas path and the screet kay
+
 class Config:
-    SECRET_KEY = "INDF87fdff"
-    SQLALCHEMY_DATABASE_URI = "postgres://kqayzjyrejhqsi:25de8e8a84a58cdc7b74624e94398dba8270f229285dfaf1f7b8872576948a46@ec2-50-17-255-244.compute-1.amazonaws.com:5432/ddift5mi07vt0q"
+    SECRET_KEY = "INDF87fdff"    
+    SQLALCHEMY_DATABASE_URI = "postgresql://kqayzjyrejhqsi:25de8e8a84a58cdc7b74624e94398dba8270f229285dfaf1f7b8872576948a46@ec2-50-17-255-244.compute-1.amazonaws.com:5432/ddift5mi07vt0q"
     MYSQL_DATABASE_CHARSET = 'utf8mb4'
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_DEFAULT_SENDER = "IEEENB@gmail.com"
